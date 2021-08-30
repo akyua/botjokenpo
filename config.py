4eae6e3a8c905f13654da9061d5acf9d22a2f935
akyua <75745796+akyua@users.noreply.github.com>
2021-08-29 23:08:55 -0300

    Initialize dev

---
+++ config.py
@@ -0,0 +1,31 @@
+import logging
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
+
+# Backend: Discord
+BACKEND = 'Discord'
+BOT_IDENTITY = {
+    # Necessário o arquivo .env no formato:
+    # DISCORD_TOKEN=<token>
+    # onde <token> contém o valor obtido de
+    # https://discord.com/developers/applications
+    'token': environ.get('DISCORD_TOKEN')
+}
+BOT_ADMINS = (environ.get('BOT_ADMIN'), )
+
+# Diretórios do Errbot
+BOT_DATA_DIR = r'data'
+BOT_EXTRA_PLUGIN_DIR = r'plugins'
+BOT_EXTRA_BACKEND_DIR = r'backend'
+
+# Log
+BOT_LOG_FILE = r'errbot.log'
+BOT_LOG_LEVEL = logging.DEBUG
+
+# Se os plugins têm dependências, usar ambiente virtual e requirements.txt
+AUTOINSTALL_DEPS = True
+
+# Prefixo do bot
+BOT_PREFIX = '!'
+BOT_PREFIX_OPTIONAL_ON_CHAT = True
